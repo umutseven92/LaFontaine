8c235df8dd96c30dffc2963ea2c537c96c29203b
Umut Seven <umutseven92@gmail.com>
2018-12-15 03:47:28 +0300

    Remove unused test stub

--- test/unit/test_generator.py
+++ test/unit/test_generator.py
@@ -1,19 +1,1 @@
-import cv2
-import os
-from lafontaine.generator.video_generator import VideoGenerator
 
-"""
-def test_can_generate_from_scene():
-    output_path = '../../out/result_from_scene.avi'
-
-    if os.path.exists(output_path):
-        os.remove(output_path)
-
-    video_generator = VideoGenerator()
-    video_generator.generate_from_scene(images, output_path)
-
-    cap = cv2.VideoCapture(output_path)
-    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    assert video_length == FRAME_AMOUNT
-"""
